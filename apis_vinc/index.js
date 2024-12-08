98e4f030f80cbd24ec9666e2da89f070bcb36b6e
Chelsy Mutua <mutuachelsy@gmail.com>
2024-12-08 09:13:15 +0300

    Change the supabase initilization to use anon key

--- apis_vinc/index.js
+++ apis_vinc/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 // Initialize Supabase client
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 
